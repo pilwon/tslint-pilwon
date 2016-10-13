6879bae46e7f076f27fdbf34f7edaea699867ff7
Pilwon Huh <pilwon@gmail.com>
2016-10-13 16:00:18 +0800

    Bumped to v0.1.3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   "extends": "tslint:latest",
   "rules": {
+    "interface-name": [false, "always-prefix"],
     "no-console": [false],
     "no-var-requires": false,
     "quotemark": [true, "single", "avoid-escape"]
