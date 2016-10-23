d9a71fb1f1b9c4e08f42dc2a7e302210e3accfbc
Pilwon Huh <pilwon@gmail.com>
2016-10-23 15:40:30 +0800

    Added allow-leading-underscore

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ module.exports = {
     "interface-name": [false, "always-prefix"],
     "no-console": [false],
     "no-var-requires": false,
-    "quotemark": [true, "single", "avoid-escape"]
+    "quotemark": [true, "single", "avoid-escape"],
+    "variable-name": [
+      true,
+      "ban-keywords",
+      "check-format",
+      "allow-leading-underscore",
+      "allow-pascal-case"
+    ],
   }
 };
