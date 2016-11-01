339cd89934442d67a78574aef48e21d74f0bd78f
Pilwon Huh <pilwon@gmail.com>
2016-11-02 00:09:11 +0700

    Disabled member-access

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   "extends": "tslint:latest",
   "rules": {
     "interface-name": [false, "always-prefix"],
+    "member-access": false,
     "no-console": [false],
     "no-var-requires": false,
     "quotemark": [true, "single", "jsx-double", "avoid-escape"],
